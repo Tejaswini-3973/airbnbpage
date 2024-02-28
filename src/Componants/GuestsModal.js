aee349e5bc023d86a5e296f025cac6212102bbf7
Tejaswini Patil <tejaswinipatil316@gmail.com>
2024-02-28 15:43:54 +0530

    title changes

--- src/Componants/GuestsModal.js
+++ src/Componants/GuestsModal.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 const GuestsModal = ({ show, handleClose }) => {
   return (
+
     <div className={`modal ${show ? 'show' : ''}`} tabIndex="-1" role="dialog" style={{ display: show ? 'block' : 'none' }}>
       <div className="modal-dialog" role="document">
+        
         <div className="modal-content">
           <div className="modal-header">
-            <h5 className="modal-title">Guests Selection</h5>
+            <h5 className="modal-title">Guests</h5>
+           
             <button type="button" className="btn-close" aria-label="Close" onClick={handleClose}></button>
           </div>
+          
           <div className="modal-body">
             <p>Adults: <input type="number" defaultValue="1" /></p>
             <p>Children: <input type="number" defaultValue="0" /></p>
